3ceb245c7f2ef412d2be1cfb222f0fc81452ae74
Patajuade <manon.diana@icloud.com>
2022-03-05 00:46:43 +0100

    add new embed command

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # iot-project-2022-discordjs
+
+à faire :
+- Commande qui renvoie toutes les personnes qu'il y a dans le même serveur que moi (soit dans un console log, soit il le reply)
+- Faire un beau message avec un embed ( mocker les données -> comme si c'était des vraies données mais hard codées )
+- Faire dire le résultat du roll en TTS
--- commands/insult.js
+++ commands/insult.js
@@ -1,11 +1,20 @@
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { Guild } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('répondencué')
+		.setName('répondenculé')
 		.setDescription('Allows you to trigger the bots anger issues'),
 	async execute(interaction) {
+		interaction.member.guild.channels.fetch().then(channels => {
+			//channels.map(channel => channel.members.map(member => console.log(member)));
+			const voiceChannels = channels.filter(channel=>channel.type==='GUILD_VOICE');
+			//console.log(voiceChannels);
+			const currentVoiceChannel = voiceChannels.filter(vc=>vc.members.filter(member=>member.user.username===interaction.user.username));
+			const members = currentVoiceChannel.map(cvc=>cvc.members);
+			members.map(m=>console.log(m.username));
+			//console.log(currentVoiceChannel);
+		})
 		await interaction.reply(`Nique ta mère ${interaction.user.tag} ! `);
 	},
 };
